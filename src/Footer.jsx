80f0296faad77dd191df0ca8e810495a7f16df73
Sayam2801 <sayamk565@gmail.com>
2021-03-20 18:15:49 +0530

    first-commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    return <footer><p>Copyright © {currentYear}</p></footer>
+}
+
+export default Footer;
